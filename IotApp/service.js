1e523f846a96aa8eec1063df6f895f300f1d6670
unknown <jdh9797@knu.ac.kr>
2021-07-28 14:41:54 +0900

    update

--- IotApp/service.js
+++ IotApp/service.js
@@ -10,7 +10,8 @@ function init_gpio(){
     function callback(message){
         var respond=JSON.parse(message);
         if(pin in respond.gpioList && respond.gpioList[pin].status=="available"){
-            gpioOpen();    
+            gpioOpen();
+            gpioOutlow();
         }
     }
     
@@ -29,7 +30,21 @@ function gpioOpen(){
     }
 
     bridge.call(luna+'open',param);
-}
+};
+
+function gpioOutlow(){
+    var bridge=new WebOSServiceBridge();
+    var param=JSON.stringify(`{"pin":${pin}, "Direction":"outLow"}`);
+
+    bridge.onservicecallback=callback;
+
+    function callback(message){
+        var respond=JSON.parse(message);
+        console.log("open_respond:"+respond.returnValue);
+    }
+
+    bridge.call(luna+'setDirection',param);
+};
 
 function light(){
     var bridge=new WebOSServiceBridge();
@@ -39,40 +54,41 @@ function light(){
     
     function callback(message){
         var respond=JSON.parse(message);
-        if(respond.returnValue){
-            if(getDirect()){
-
-            }
+        if(respond.value=="low"){
+            turn_on();
         }
         else{
-            init_gpio();
+            turn_off();
         }
     }
     
     bridge.call(luna+'getValue',param);
-}
-function getDirect(){
+};
+function turn_on(){
     var bridge=new WebOSServiceBridge();
-    var param=JSON.stringify(`{"pin":${pin}}`);
-    
+    var param=JSON.stringify(`{"pin":${pin}, "value":"highj"}`);
+
     bridge.onservicecallback=callback;
-    
+
     function callback(message){
         var respond=JSON.parse(message);
-        if(respond.returnValue && respond.direction=="out"){
-            return true;
-        }
-        else{
-            console.log("direction error");
-            return false;
-        }
+        console.log("open_respond:"+respond.returnValue);
     }
-    
-    bridge.call(luna+'getDirection',param);
-}
-function turn_on(){
 
-}
+    bridge.call(luna+'setValue',param);
+};
 function turn_off(){
-    
-}
+    var bridge=new WebOSServiceBridge();
+    var param=JSON.stringify(`{"pin":${pin}, "value":"low"}`);
+
+    bridge.onservicecallback=callback;
+
+    function callback(message){
+        var respond=JSON.parse(message);
+        console.log("open_respond:"+respond.returnValue);
+    }
+
+    bridge.call(luna+'setValue',param);
+};
+
+init_gpio();
